59602a802a0016a1e298d7b784aa34516f7bd8fa
Alexey-zaliznuak <zaliznuak20@gmail.com>
2023-07-20 13:05:19 +0300

    create EmailConfirmed permission error message

--- AutoDocsOnlineAPI/users/permissions.py
+++ AutoDocsOnlineAPI/users/permissions.py
@@ -6,6 +6,9 @@ class EmailConfirmed(permissions.BasePermission):
     """
     Get user by username and return True if he confirmed email
     """
+
+    message = 'You have not confirmed your email.'
+
     def has_permission(self, request, view):
         q = User.objects.filter(username=request.data.get('username'))
         return q.exists() and q.get().email_confirmed
