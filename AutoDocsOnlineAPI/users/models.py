476ddb560e35a7d5572f366b64a09d3a44eac35b
Alexey-zaliznuak <zaliznuak20@gmail.com>
2023-07-22 22:17:30 +0300

    recreate database, return 'id' in user model

--- AutoDocsOnlineAPI/users/models.py
+++ AutoDocsOnlineAPI/users/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
     username = models.CharField(
         _('username'),
         max_length=settings.USER_USERNAME_MAX_LENGTH,
-        primary_key=True,
+        unique=True,
         help_text=_(
             'Required. 150 characters or fewer. '
             'Letters, digits and @/./+/-/_ only.'
