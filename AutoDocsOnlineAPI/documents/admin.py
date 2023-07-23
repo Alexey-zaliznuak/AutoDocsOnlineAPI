1ac6345643f41f17bcfcfa8d4c34bdc9aaf62b12
Alexey-zaliznuak <zaliznuak20@gmail.com>
2023-07-23 11:27:40 +0300

    add UserTemplateValueAdmin

--- AutoDocsOnlineAPI/documents/admin.py
+++ AutoDocsOnlineAPI/documents/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
 
-from .models import Template
+from .models import Template, UserTemplateValue
 
 
 @admin.register(Template)
 class TemplateAdmin(admin.ModelAdmin):
     list_display = ('title', 'is_official', 'name_in_document', 'description',)
     list_filter = ('is_official',)
+    search_fields = ('title', 'name_in_document', 'description',)
+
+
+@admin.register(UserTemplateValue)
+class UserTemplateValueAdmin(admin.ModelAdmin):
+    list_display = ('user', 'template', 'value', 'pk',)
     search_fields = ('title', 'name_in_document', 'description',)
