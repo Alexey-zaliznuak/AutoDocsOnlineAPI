b7ae60ef2e2610a02b2fd00f74982ac9aa02fa77
Alexey-zaliznuak <zaliznuak20@gmail.com>
2023-07-28 23:18:22 +0300

    add object_url function getting link on object in admin panel

---
+++ AutoDocsOnlineAPI/core/admin.py
@@ -0,0 +1,16 @@
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
+
+def object_url(obj) -> str:
+    """
+    Return mark save html link on edit object in django admin panel.
+    """
+    url = reverse(
+        f'admin:{obj._meta.app_label}'
+        f'_{obj._meta.object_name.lower()}_change',
+        kwargs={'object_id': obj.pk}
+    )
+    return mark_safe(
+        f'<a target="_blank" href={url}>{obj}</a>'
+    )
