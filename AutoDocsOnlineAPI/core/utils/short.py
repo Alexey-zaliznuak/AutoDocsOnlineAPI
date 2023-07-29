3bed75d29ac015799de202b356ae5698f3379c4f
Alexey-zaliznuak <zaliznuak20@gmail.com>
2023-07-29 19:21:28 +0300

    update short function

--- AutoDocsOnlineAPI/core/utils/short.py
+++ AutoDocsOnlineAPI/core/utils/short.py
@@ -1,7 +1,14 @@
 from django.conf import settings
 
 
-def short(text, max_length):
+def short(
+        text: str,
+        max_length: int = 20,
+        short_end: str = settings.SHORT_END
+) -> str:
+    """
+    if necessary, truncates the string to the specified value.
+    """
     if len(text) > max_length:
-        return text[:max_length] + settings.SHORT_END
+        return text[:max_length] + short_end
     return text
