ff859cffdcb4b314b7ee2b247877175a8618f93f
Alexey-zaliznuak <zaliznuak20@gmail.com>
2023-07-29 19:14:52 +0300

    update object_url function

--- AutoDocsOnlineAPI/core/admin.py
+++ AutoDocsOnlineAPI/core/admin.py
@@ -2,9 +2,11 @@ from django.urls import reverse
 from django.utils.safestring import mark_safe
 
 
-def object_url(obj) -> str:
+def object_url(obj, title) -> str:
     """
     Return mark save html link on edit object in django admin panel.
+    :param title string - link on obj
+    :param obj for create link on it
     """
     url = reverse(
         f'admin:{obj._meta.app_label}'
@@ -12,5 +14,5 @@ def object_url(obj) -> str:
         kwargs={'object_id': obj.pk}
     )
     return mark_safe(
-        f'<a target="_blank" href={url}>{obj}</a>'
+        f'<a target="_blank" href={url}>{title or obj}</a>'
     )
