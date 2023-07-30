662d4d93832b49538305ccf56410d6f5e8e6f394
Alexey-zaliznuak <zaliznuak20@gmail.com>
2023-07-30 17:27:12 +0300

    update CreatedModel docstring

--- AutoDocsOnlineAPI/core/models.py
+++ AutoDocsOnlineAPI/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class CreatedModel(models.Model):
-    """Abstarct model. Add 'created' field."""
+    """Abstarct model. Add 'creation_date' field."""
 
     creation_date = models.DateTimeField(
         "Date of creation",
