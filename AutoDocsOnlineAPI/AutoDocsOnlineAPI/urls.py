cb9be38d184780276bb5805c4c53c61c39a8e829
Alexey-zaliznuak <zaliznuak20@gmail.com>
2023-08-06 21:27:07 +0300

    add link on file in DocumentAdmin

--- AutoDocsOnlineAPI/AutoDocsOnlineAPI/urls.py
+++ AutoDocsOnlineAPI/AutoDocsOnlineAPI/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, reverse_lazy
 from django.views.generic.base import RedirectView
@@ -13,3 +15,9 @@ urlpatterns = [
         name='index'
     )
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
